b3b36b1fba9f194297a29483458aab1169bfaa83
Jeremiah <jeree@live.nl>
2014-03-20 15:08:16 +0100

    yo

---
+++ test/spec/test.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+
+(function () {
+describe(function() {
+    'use strict';
+
+        describe('maybe a bit more context here', function () {
+            it('should run here few assertions', function () {
+
+                expect(1).toBe(1);
+            });
+        });
+    });
+})();
